4d52b60bbcde9314a0b2ffd6980320cdc8635ce5
dmkilday <BeautifulJesu5>
2016-11-23 13:53:39 -0700

    Moved cucumber files to src/test directory.

--- src/test/java/com/harvard/group3/cucumber/CucumberRunner.java
+++ src/test/java/com/harvard/group3/cucumber/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/main/java/com/harvard/group3/cucumber"}
+        features = {"src/test/java/com/harvard/group3/cucumber"}
 )
 public class CucumberRunner {
 }
